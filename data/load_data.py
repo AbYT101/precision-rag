91d99fbd23aae2a972c160bf855e188fbc7049a2
Abraham Y. <aberhamyirsaw@gmail.com>
2024-06-04 22:30:35 +0300

    .env file added

--- data/load_data.py
+++ data/load_data.py
@@ -5,7 +5,11 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Weaviate
 import weaviate
 from weaviate.embedded import EmbeddedOption
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
 
 # Load Data 
 url = ""
@@ -21,10 +25,14 @@ text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 chunks = text_splitter.split_documents(documents)
 
 # Embed and store the chunks
+weaviate_url = os.getenv('WEAVIATE_INSTANCE_URL')
+weaviate_pwd = os.getenv('WEAVIATE_PASSWORD')
+weaviate_usr = os.getenv('WEAVIATE_USERNAME')
+
 client = weaviate.Client(
   embedded_options = EmbeddedOptions(),
-  url='YOUR_WEAVIATE_INSTANCE_URL',
-  auth={'username': 'YOUR_USERNAME', 'password': 'YOUR_PASSWORD'}
+  url= weaviate_url,
+  auth={'username': weaviate_usr, 'password': weaviate_pwd }
 )
 
 vectorstore = Weaviate.from_documents(
